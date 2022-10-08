eb353e246b3d750bd4da04a499a59a7585c2071c
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-08 16:03:20 +0530

    used route instance as middleware

--- CoddingAddict/methods/app.js
+++ CoddingAddict/methods/app.js
@@ -1,35 +1,14 @@
 const express = require('express');
 const app = express();
-let { products } = require('./data');
+let products = require('./routes/products.js')
 
-// app.use(express.urlencoded({ extended: false }));
 
+// parse form data for all routes
+app.use(express.urlencoded({ extended: false }));
+
+// parse json data for all routes
 app.use(express.json());
 
-app.get('/api/products', (req, res) => {
-    res.status(200).json({ data: products });
-})
-
-app.post('/api/products', (req, res) => {
-    products.push(req.body);
-    console.log(req.body);
-    console.log(products);
-    res.sendStatus(200);
-});
-
-app.put('/api/products', (req, res) => {
-    products.find((ele) => {
-        if (ele.id === req.body.id) {
-            const { name, image, price, desc } = req.body;
-            ele.name = name;
-            ele.image = image;
-            ele.price = price;
-            ele.desc = desc;
-        }
-    })
-    console.log(products);
-    res.sendStatus(200);
-
-});
+app.use('/api/products', products)
 
 app.listen(5000);
